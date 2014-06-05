a5d2e8fac8520ffa102b10e436e4fbea30ca8693
Jesse Kade <jessekade@gmail.com>
2014-06-05 16:56:15 -0700

    Finish auditions page

--- content/themes/harris-studios/archive-auditions.php
+++ content/themes/harris-studios/archive-auditions.php
@@ -4,40 +4,46 @@
 */
 ?>
 <?php get_template_part('templates/page', 'header'); ?>
+<div class="container text-center">
+  <div class="row">
+    <div class="col-xs-8 col-xs-offset-2">
+      <?php if (!have_posts()) : ?>
+        <div class="alert alert-warning">
+          <?php _e('Sorry, no results were found.', 'roots'); ?>
+        </div>
+        <?php get_search_form(); ?>
+      <?php endif; ?>
+      <?php while (have_posts()) : the_post(); ?>
+        <?php //get_template_part('templates/content', get_post_format()); ?>
 
-<?php if (!have_posts()) : ?>
-  <div class="alert alert-warning">
-    <?php _e('Sorry, no results were found.', 'roots'); ?>
-  </div>
-  <?php get_search_form(); ?>
-<?php endif; ?>
-<?php while (have_posts()) : the_post(); ?>
-  <?php //get_template_part('templates/content', get_post_format()); ?>
+        <article <?php post_class(); ?>>
+          <header>
+            <h2 class="entry-title"><?php the_title(); ?></h2>
+            <?php //get_template_part('templates/entry-meta'); ?>
+            <p class="lead">
+              <strong><?php the_field('audition_starting_date'); ?> @ <?php the_field('audition_start_time'); ?></strong>
+            </p>
+            <address>
+              <p><strong>Where:</strong></p>
+              <?php the_field('audition_location'); ?>
+            </address>
+          </header>
+          <div class="entry-summary">
+            <p><strong>Details:</strong></p>
+            <?php the_content(); ?>
+          </div>
+        </article>
+        <hr>
+      <?php endwhile; ?>
 
-  <article <?php post_class(); ?>>
-    <header>
-      <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-      <?php //get_template_part('templates/entry-meta'); ?>
-      <p class="lead">
-        <strong><?php the_field('audition_starting_date'); ?> @ <?php the_field('audition_start_time'); ?></strong>
-      </p>
-      <address>
-        <?php the_field('audition_location'); ?>
-      </address>
-    </header>
-    <div class="entry-summary">
-      <p><strong>Details:</strong></p>
-      <?php the_excerpt(); ?>
+      <?php if ($wp_query->max_num_pages > 1) : ?>
+        <nav class="post-nav">
+          <ul class="pager">
+            <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
+            <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
+          </ul>
+        </nav>
+      <?php endif; ?>
     </div>
-  </article>
-  <hr>
-<?php endwhile; ?>
-
-<?php if ($wp_query->max_num_pages > 1) : ?>
-  <nav class="post-nav">
-    <ul class="pager">
-      <li class="previous"><?php next_posts_link(__('&larr; Older posts', 'roots')); ?></li>
-      <li class="next"><?php previous_posts_link(__('Newer posts &rarr;', 'roots')); ?></li>
-    </ul>
-  </nav>
-<?php endif; ?>
+  </div>
+</div>
