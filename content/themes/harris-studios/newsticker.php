f744fe5a0248291043b4b6e8567307a67e9158f4
Jesse Kade <jessekade@gmail.com>
2014-06-25 16:11:23 -0700

    Finish news ticker function

--- content/themes/harris-studios/newsticker.php
+++ content/themes/harris-studios/newsticker.php
@@ -1,7 +1,14 @@
 <?php if (is_front_page()): ?>
   <div class="newsticker">
     <div class="container">
-      <strong>What's Next?:</strong> <a href="#"><?php echo wp_trim_words("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit eos itaque voluptatem autem dolor eligendi cum quam perferendis cupiditate odio.", 10 ); ?> <i class="fa fa-caret-right"></i></a>
+      <?php $args = array( 'post_type' => 'newsticker','showposts' => 1); ?>
+      <?php $newsticker = new WP_Query( $args ); ?>
+      <?php if ( $newsticker->have_posts() ) : ?>
+        <?php while ($newsticker->have_posts()) : $newsticker->the_post(); ?>
+          <strong><i class="fa fa-calendar"></i> What's Next?:</strong> <a href="<?php the_field('links_to'); ?>"><?php echo wp_trim_words(get_the_title(), 10 ); ?> <i class="fa fa-caret-right"></i></a>
+          <span class="pull-right"><a href="#music">More <i class="fa fa-caret-down"></i></a></span>
+        <?php endwhile; ?>
+      <?php endif; ?>
     </div>
   </div>
 <?php endif; ?>
