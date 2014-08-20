1dd02bbb071d8c106aaf2c5a7e67f90f75caf23b
Jesse Kade <jessekade@gmail.com>
2014-08-20 16:43:52 -0700

    Add ability for newsticker to link to external sites

--- web/app/themes/harris-studios/newsticker.php
+++ web/app/themes/harris-studios/newsticker.php
@@ -5,7 +5,15 @@
       <?php $newsticker = new WP_Query( $args ); ?>
       <?php if ( $newsticker->have_posts() ) : ?>
         <?php while ($newsticker->have_posts()) : $newsticker->the_post(); ?>
-          <strong><i class="fa fa-calendar"></i> What's Next?:</strong> <a href="<?php the_field('links_to'); ?>"><?php echo wp_trim_words(get_the_title(), 10 ); ?> <i class="fa fa-caret-right"></i></a>
+          <strong><i class="fa fa-calendar"></i> What's Next?:</strong>
+          <?php if (get_field('external_link')): ?>
+            <a href="http://<?php the_field('external_link'); ?>">
+          <?php else: ?>
+            <a href="<?php the_field('links_to'); ?>">
+          <?php endif; ?>
+              <?php echo wp_trim_words(get_the_title(), 10 ); ?>
+              <i class="fa fa-caret-right"></i>
+            </a>
           <span class="pull-right"><a href="#music">More <i class="fa fa-caret-down"></i></a></span>
         <?php endwhile; ?>
       <?php endif; ?>
